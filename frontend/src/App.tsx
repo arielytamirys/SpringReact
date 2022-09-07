5a24cd33afb6f915431051d7e80cbd80094998fb
ariely tamirys <arielytamirys.la@gmail.com>
2022-09-07 11:26:28 -0300

    primeiro

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import NotifictionButtons from "./compomentes/NotifictionButtons"
+
+
+function App() {
+  return(
+    <>
+<h1>Ola mundo </h1>
+<NotifictionButtons/>
+
+</>  
+  )
+}
+
+export default App
---
+++ frontend/src/compomentes/NotifictionButtons/index.tsx
@@ -0,0 +1,15 @@
+import icon from '../../assets/imgs/vector.svg'
+import './style.css'
+
+function  NotifictionButtons() {
+    return(
+        
+        <div className="dsmeta-red-btn">
+            <img src= {icon} alt="Notificar" />
+        </div>
+      
+        
+    )
+
+}
+export default NotifictionButtons;
