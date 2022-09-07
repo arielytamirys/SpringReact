49231d4004852fd08ed0b151cf46fe547b235794
ariely tamirys <arielytamirys.la@gmail.com>
2022-09-07 16:52:09 -0300

    compomentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import Header from "./compomentes/Header"
 import NotifictionButtons from "./compomentes/NotifictionButtons"
+import SalesCard from "./compomentes/SalesCard"
 
 
 function App() {
   return(
     <>
-<h1>Ola mundo </h1>
-<NotifictionButtons/>
+<h4>Feito por Ariely Santos, com as aulas da DevSuperior.  </h4>
+<Header/>
+
+<main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard/>
+
+        </div>
+      </section>
+</main>
 
 </>  
   )
